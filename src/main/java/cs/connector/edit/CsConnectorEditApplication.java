79cbaf61069d92e01b68ca9e16a050050486abac
micnori <micnori@yahoo.com>
2022-01-18 09:39:02 +0100

    edit connector mockup

---
+++ src/main/java/cs/connector/edit/CsConnectorEditApplication.java
@@ -0,0 +1,13 @@
+package cs.connector.edit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsConnectorEditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsConnectorEditApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cs/connector/edit/controller/MainController.java
@@ -0,0 +1,39 @@
+package cs.connector.edit.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+public class MainController {
+	
+	@GetMapping("/educatinalActivity")
+	public ResponseEntity<List<Map<String, Object>>> getSAAEducatinalActivity(
+			@RequestParam String fiscalCode,
+			@RequestParam String entityType, 
+			@RequestParam String viewName) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		ResourceLoader resourceLoader = new DefaultResourceLoader();
+		Resource resource = null;
+		try {
+			resource = resourceLoader.getResource("classpath:json/educatinalActivity.json");
+			List<Map<String,Object>> list = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<Map<String, Object>>>() {});
+			return new ResponseEntity<List<Map<String, Object>>>(list, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<List<Map<String, Object>>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+		
+}
